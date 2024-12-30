3596d87da87f329df9debfd7d3ec6114c73b2054
nerdchanii <nerdchanii@gmail.com>
2024-12-30 14:55:15 +0900

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import todoRoutes from "./routes/todo.routes";
+import cors from "cors";
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
